946c7c384157a79c90f4ac76817b52916af150e9
Steven <steven.infinity29@gmail.com>
2021-01-07 01:50:52 -0500

    fix /customer endpoints

--- main.go
+++ main.go
@@ -1,33 +1,36 @@
 package main
 
 import (
-    "context"
-    "github.com/spf13/viper"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
-    "github.com/steven-steven/GoInvoice/config"
-    "github.com/steven-steven/GoInvoice/invoice"
-    "github.com/steven-steven/GoInvoice/item"
+	"context"
+	"github.com/spf13/viper"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"github.com/steven-steven/GoInvoice/config"
+	"github.com/steven-steven/GoInvoice/invoice"
+	"github.com/steven-steven/GoInvoice/item"
+	"github.com/steven-steven/GoInvoice/customer"
 )
 
 type invoiceEndpoint = invoice.Endpoints
 type itemEndpoint = item.Endpoints
+type customerEndpoint = customer.Endpoints
 
-type combinedEndpoint struct {
-    *invoiceEndpoint
-    *itemEndpoint
+	type combinedEndpoint struct {
+	*invoiceEndpoint
+	*itemEndpoint
+	*customerEndpoint
 }
 
 func main() {
-    viper.BindEnv("port")
-    var httpAddr = viper.GetString("port")
-    if httpAddr == "" {
-        httpAddr = "8080"
-    }
+	viper.BindEnv("port")
+	var httpAddr = viper.GetString("port")
+	if httpAddr == "" {
+			httpAddr = "8080"
+	}
     
 	ctx := context.Background()
 	errChan := make(chan error)
@@ -38,38 +41,45 @@ func main() {
 	}
 	
 	// INVOICE SERVICE
-    srv := invoice.NewService(*dbClient)
-    // ITEM SERVICE
-    srvItem := item.NewService(*dbClient)
+	srv := invoice.NewService(*dbClient)
+	// ITEM SERVICE
+	srvItem := item.NewService(*dbClient)
+	// CUSTOMER SERVICE
+	srvCustomer := customer.NewService(*dbClient)
 
-    go func() {	// cntrl-C
-        c := make(chan os.Signal, 1)
-        signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-        errChan <- fmt.Errorf("%s", <-c)
-    }()
+	go func() {	// cntrl-C
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
 
-    // mapping endpoints
-    endpoints := combinedEndpoint{
-        &invoice.Endpoints{
-            PostInvoiceEndpoint:	invoice.MakePostInvoiceEndpoint(srv),
-            GetInvoiceEndpoint:   	invoice.MakeGetInvoiceEndpoint(srv),
-            PutInvoiceEndpoint: 	invoice.MakePutInvoiceEndpoint(srv),
-            DeleteInvoiceEndpoint: 	invoice.MakeDeleteInvoiceEndpoint(srv),
-            GetAllInvoiceEndpoint: 	invoice.MakeGetAllInvoiceEndpoint(srv),
-        },
-        &item.Endpoints{
-            PostItemEndpoint:	    item.MakePostItemEndpoint(srvItem),
-            DeleteItemEndpoint: 	item.MakeDeleteItemEndpoint(srvItem),
-            GetAllItemEndpoint: 	item.MakeGetAllItemEndpoint(srvItem),
-        },
+	// mapping endpoints
+	endpoints := combinedEndpoint{
+		&invoice.Endpoints{
+				PostInvoiceEndpoint:	invoice.MakePostInvoiceEndpoint(srv),
+				GetInvoiceEndpoint:   	invoice.MakeGetInvoiceEndpoint(srv),
+				PutInvoiceEndpoint: 	invoice.MakePutInvoiceEndpoint(srv),
+				DeleteInvoiceEndpoint: 	invoice.MakeDeleteInvoiceEndpoint(srv),
+				GetAllInvoiceEndpoint: 	invoice.MakeGetAllInvoiceEndpoint(srv),
+		},
+		&item.Endpoints{
+				PostItemEndpoint:	    item.MakePostItemEndpoint(srvItem),
+				DeleteItemEndpoint: 	item.MakeDeleteItemEndpoint(srvItem),
+				GetAllItemEndpoint: 	item.MakeGetAllItemEndpoint(srvItem),
+		},
+		&customer.Endpoints{
+			PostCustomerEndpoint:	    customer.MakePostCustomerEndpoint(srvCustomer),
+			DeleteCustomerEndpoint: 	customer.MakeDeleteCustomerEndpoint(srvCustomer),
+			GetAllCustomerEndpoint: 	customer.MakeGetAllCustomerEndpoint(srvCustomer),
+		},
 	}
 
-    // HTTP transport
-    go func() {
-        log.Println("app listening on port:", httpAddr)
-        handler := newHTTPServer(ctx, endpoints)
-        errChan <- http.ListenAndServe(":" + httpAddr, handler)
-    }()
+	// HTTP transport
+	go func() {
+		log.Println("app listening on port:", httpAddr)
+		handler := newHTTPServer(ctx, endpoints)
+		errChan <- http.ListenAndServe(":" + httpAddr, handler)
+	}()
 
-    log.Fatalln(<-errChan)
+	log.Fatalln(<-errChan)
 }
--- server.go
+++ server.go
@@ -1,78 +1,109 @@
 package main
 
 import (
-    "context"
-    "net/http"
+	"context"
+	"net/http"
 
-    httptransport "github.com/go-kit/kit/transport/http"
-    "github.com/gorilla/mux"
-    "github.com/steven-steven/GoInvoice/invoice"
-    "github.com/steven-steven/GoInvoice/item"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	"github.com/steven-steven/GoInvoice/invoice"
+	"github.com/steven-steven/GoInvoice/item"
+	"github.com/steven-steven/GoInvoice/customer"
 )
 
 func newHTTPServer(ctx context.Context, endpoints combinedEndpoint) http.Handler {
-    r := mux.NewRouter()
-    r.Use(commonMiddleware)
+	r := mux.NewRouter()
+	r.Use(commonMiddleware)
 
-    r.Methods("POST").Path("/invoice").Handler(httptransport.NewServer(
-        endpoints.PostInvoiceEndpoint,
-        invoice.DecodePostInvoiceRequest,
-        invoice.EncodeResponse,
-    ))
+	r.Methods("POST").Path("/invoice").Handler(httptransport.NewServer(
+		endpoints.PostInvoiceEndpoint,
+		invoice.DecodePostInvoiceRequest,
+		invoice.EncodeResponse,
+	))
 
-    r.Methods("GET").Path("/invoice/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
-        endpoints.GetInvoiceEndpoint,
-        invoice.DecodeGetInvoiceRequest,
-        invoice.EncodeResponse,
-    ))
+	r.Methods("GET").Path("/invoice/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
+		endpoints.GetInvoiceEndpoint,
+		invoice.DecodeGetInvoiceRequest,
+		invoice.EncodeResponse,
+	))
 
-    r.Methods("PUT").Path("/invoice/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
-        endpoints.PutInvoiceEndpoint,
-        invoice.DecodePutInvoiceRequest,
-        invoice.EncodeResponse,
+	r.Methods("PUT").Path("/invoice/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
+		endpoints.PutInvoiceEndpoint,
+		invoice.DecodePutInvoiceRequest,
+		invoice.EncodeResponse,
 	))
 	
 	r.Methods("DELETE").Path("/invoice/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
-        endpoints.DeleteInvoiceEndpoint,
-        invoice.DecodeDeleteInvoiceRequest,
-        invoice.EncodeResponse,
-    ))
+		endpoints.DeleteInvoiceEndpoint,
+		invoice.DecodeDeleteInvoiceRequest,
+		invoice.EncodeResponse,
+	))
 
 	r.Methods("GET").Path("/allInvoice").Handler(httptransport.NewServer(
-        endpoints.GetAllInvoiceEndpoint,
-        invoice.DecodeGetAllInvoiceRequest,
-        invoice.EncodeResponse,
+		endpoints.GetAllInvoiceEndpoint,
+		invoice.DecodeGetAllInvoiceRequest,
+		invoice.EncodeResponse,
 	))
     
-    // ITEM
+	// ITEM
 
-    r.Methods("POST").Path("/item").Handler(httptransport.NewServer(
-        endpoints.PostItemEndpoint,
-        item.DecodePostItemRequest,
-        item.EncodeResponse,
-    ))
+	r.Methods("POST").Path("/item").Handler(httptransport.NewServer(
+		endpoints.PostItemEndpoint,
+		item.DecodePostItemRequest,
+		item.EncodeResponse,
+	))
 	
 	r.Methods("DELETE").Path("/item/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
-        endpoints.DeleteItemEndpoint,
-        item.DecodeDeleteItemRequest,
-        item.EncodeResponse,
-    ))
+		endpoints.DeleteItemEndpoint,
+		item.DecodeDeleteItemRequest,
+		item.EncodeResponse,
+	))
 
 	r.Methods("GET").Path("/allItem").Handler(httptransport.NewServer(
-        endpoints.GetAllItemEndpoint,
-        item.DecodeGetAllItemRequest,
-        item.EncodeResponse,
-    ))
+		endpoints.GetAllItemEndpoint,
+		item.DecodeGetAllItemRequest,
+		item.EncodeResponse,
+	))
     
+	// CUSTOMER
 
+	r.Methods("POST").Path("/customer").Handler(httptransport.NewServer(
+		endpoints.PostCustomerEndpoint,
+		customer.DecodePostCustomerRequest,
+		customer.EncodeResponse,
+	))
+
+	r.Methods("GET").Path("/customer/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
+		endpoints.GetCustomerEndpoint,
+		customer.DecodeGetCustomerRequest,
+		customer.EncodeResponse,
+	))
+
+	r.Methods("PUT").Path("/customer/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
+		endpoints.PutCustomerEndpoint,
+		customer.DecodePutCustomerRequest,
+		customer.EncodeResponse,
+	))
+
+	r.Methods("DELETE").Path("/customer/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}").Handler(httptransport.NewServer(
+		endpoints.DeleteCustomerEndpoint,
+		customer.DecodeDeleteCustomerRequest,
+		customer.EncodeResponse,
+	))
+
+	r.Methods("GET").Path("/allCustomer").Handler(httptransport.NewServer(
+		endpoints.GetAllCustomerEndpoint,
+		customer.DecodeGetAllCustomerRequest,
+		customer.EncodeResponse,
+	))
 	
-    return r
+	return r
 }
 
 func commonMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Add("Content-Type", "application/json")
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
 
